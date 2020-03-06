860ab0cc259ab24df933f75eb40f5cd7613491e1
LucasNobre <lucaas.sn@gmail.com>
2020-03-06 18:56:20 -0300

    Update http.py

--- scrapy_puppeteer/http.py
+++ scrapy_puppeteer/http.py
@@ -7,6 +7,7 @@ class PuppeteerRequest(Request):
     """Scrapy ``Request`` subclass providing additional arguments"""
 
     def __init__(self, url, callback=None, screenshot=False, wait_until=None, wait_for=None, replace_headers=True,
+                 select_element=None, action=None, send_value=None, exec_pup=None,
                  *args, **kwargs):
         """Initialize a new Puppeteer request
 
@@ -25,5 +26,9 @@ class PuppeteerRequest(Request):
         self.wait_for = wait_for
         self.screenshot = screenshot
         self.replace_headers = replace_headers
+        self.select_element = select_element
+        self.action = action
+        self.send_value = send_value
+        self.exec_pup = exec_pup
 
         super().__init__(url, callback, *args, **kwargs)
