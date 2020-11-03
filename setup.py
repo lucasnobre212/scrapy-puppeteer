0d55ab31a0c690073afee322d5dd8e4935156063
LucasNobre <lucaas.sn@gmail.com>
2020-11-03 08:12:33 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     entry_points={
         'console_scripts': [
             'scrapyp = scrapy_puppeteer.cli:scrapyp_execute',
+            'scrapy_puppeteer = scrapy_puppeteer.cli:scrapyp_execute'
         ],
     }
 )
